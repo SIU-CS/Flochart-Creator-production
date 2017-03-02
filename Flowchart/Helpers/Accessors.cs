334056dc9edb68efb1cffcb312cfc48619126422
Taylor Flatt <taylorjflatt@gmail.com>
2017-03-02 07:55:22 -0600

    Added accessors file to contain application-wide statics.

---
+++ Flowchart/Helpers/Accessors.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlowchartCreator.Helpers
+{
+    public class Accessors
+    {
+        // This may change depending on the directory structure we determine.
+        protected internal const string IP = "0.0.0.0";
+        protected internal const string FTP = "ftp://" + IP;
+
+        public static string GetFtpUrl(string username)
+        {
+            return FTP + "/flowcharts/" + username;
+        }
+    }
+}
