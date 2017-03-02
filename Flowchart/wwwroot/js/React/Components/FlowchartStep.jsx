a84dd3bf627d09ce6bf151b7b786bc638841edae
Sam Messina <sam@smessina.com>
2017-03-01 22:35:43 -0500

    Added styles for step overlay buttons

--- Flowchart/wwwroot/js/React/Components/FlowchartStep.jsx
+++ Flowchart/wwwroot/js/React/Components/FlowchartStep.jsx
@@ -9,7 +9,7 @@ class FlowchartStep extends React.Component {
         }
     }
     componentDidMount() {
-        this.setState ({
+        this.setState({
             id: this.props.id,
             title: this.props.title,
             description: this.props.description
@@ -18,9 +18,22 @@ class FlowchartStep extends React.Component {
     render() {
         return (
             <div className="flowchart-step-container">
-                <p className="flowchart-step-title">{this.state.title}</p>
-                <hr/>
-                <p className="flowchart-step-description">{this.state.description}</p>
+                <div className="flowchart-overlay">
+                    <button className="btn edit-step-btn btn-warning">
+                        <span className="glyphicon glyphicon-pencil"></span>
+                    </button>
+                    <button className="btn add-child-btn btn-success">
+                        <span className="glyphicon glyphicon-plus"></span>
+                    </button>
+                    <button className="btn delete-step-btn btn-danger">
+                        <span className="glyphicon glyphicon-trash"></span>
+                    </button>
+                </div>
+                <div className="flowchart-contents">
+                    <p className="flowchart-step-title">{this.state.title}</p>
+                    <hr />
+                    <p className="flowchart-step-description">{this.state.description}</p>
+                </div>
             </div>
         )
     }
